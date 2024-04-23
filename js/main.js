0cb975700c5281f6393f215f54df1fde34c0d132
Edwin Marulanda <roed2610@gmail.com>
2024-04-23 11:17:59 -0500

    Creación de proyecto

---
+++ js/main.js
@@ -0,0 +1,104 @@
+const formulario = document.getElementById('formulario-calculadora')
+const resultado = document.getElementById('resultado');
+const textoBienvenida = document.getElementById('text-bienvenida');
+const textoResultado = document.getElementById('text-resultado');
+const resultadoGp= document.getElementById('resultadoGp');
+const valoresTMB = { "xPeso": 10, "xAltura": 6.25, "xEdad": 5 }
+
+
+//variables
+let genero = false;
+let nombre = "";
+let tipoDocumento = "";
+let numDocumento = "";
+let edad = "";
+let peso = "";
+let altura = "";
+let actividadFisica = ""
+
+
+formulario.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    textoResultado.style="display:none";
+    asignarValoresAVariables();
+    calcularCalorias();
+
+})
+
+function asignarValoresAVariables() {
+    genero = document.getElementById('genero').checked;
+    nombre = document.getElementById('nombre').value;
+    tipoDocumento = document.getElementById('tipoDocumento').value;
+    numDocumento = document.getElementById('numeroDoc').value;
+    edad = document.getElementById('edad').value;
+    peso = document.getElementById('peso').value;
+    altura = document.getElementById('altura').value;
+    actividadFisica = document.getElementById('actividad').value;
+}
+
+function calcularCalorias() {
+    if (genero) {
+        calcularParaMasculino();
+    } else {
+        calcularParaFemenino();
+    }
+    limpiarCampos()
+}
+
+function calcularParaMasculino() {
+    //Formula hombres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) + 5
+
+}
+
+function calcularParaFemenino() {
+    //Formula mujeres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) - 161
+    let numCalorias = actividadFisica *(valoresTMB.xPeso*peso)+(valoresTMB.xAltura*altura)-(valoresTMB.xEdad*edad)-161;
+    mostrarResultado(numCalorias);
+}
+
+function mostrarResultado(numCalorias) {
+    textoBienvenida.style = "display: none !important"
+    textoResultado.style = "display: grid !important"
+    resultadoGp.innerHTML = "Grupo poblacional: "+grupoPoblacional();
+    resultado.innerHTML = `El paciente ${nombre} identificado con ${tipoDocumento} NO. ${numDocumento}, requiere un total de ${numCalorias} kcal para el sostenimiento de su TBM`
+
+}
+
+function limpiarCampos(){
+    document.getElementById('genero').checked=false;
+    document.getElementById('nombre').value= "";
+}
+
+function grupoPoblacional(){
+    return edad>=15 && edad<=29?"Joven":edad>=30&&edad<=59?"Adulto":"Adulto mayor";
+}
+
+// Animaciones
+function aparecerResultado() {
+    resultado.style.top = '100vh';
+    resultado.style.display = 'block';
+
+    let distancia = 100;
+    let resta = 0.3;
+    let id = setInterval(() => {
+        resta *= 1.1;
+        resultado.style.top = `${distancia - resta}vh`;
+        if (resta > 100) {
+            clearInterval(id);
+        }
+    }, 10)
+}
+
+function desvanecerResultado() {
+    let distancia = 1;
+
+    let id = setInterval(() => {
+        distancia *= 2;
+        resultado.style.top = `${distancia}vh`;
+        if (distancia > 100) {
+            clearInterval(id);
+            resultado.style.display = 'none';
+            resultado.style.top = 0;
+        }
+    }, 10)
+}
---
+++ js/selects.js
@@ -0,0 +1,40 @@
+
+const tiposDeDocumento = [{ "valor": "Tarjeta de identidad", "label": "Tarjeta de identidad" },
+{ "valor": "Cedula", "label": "Cédula" },
+{ "valor": "Pasaporte", "label": "Pasaporte" },
+{"valor": "Cédula de extranjeria","label": "Cédula de extranjeria"},
+]
+
+const opcionesActividadFisica=[
+{"valor":"1.2","label":"Poco o ningún ejercicio"},
+{"valor":"1.375","label":"Ejercicio ligero (1-3 días a la semana)"},
+{"valor":"1.55","label":"Ejercicio moderado (3-5 días a la semana)"},
+{"valor":"1.725","label":"Ejercicio fuerte (6-7 días a la semana)"},
+{"valor":"1.9","label":"Ejercicio muy fuerte (dos veces al día, entrenamientos muy duros)"}
+]
+
+let select = document.getElementById("tipoDocumento");
+let selectActividad = document.getElementById("actividad");
+
+function tiposDocumento() {
+    for (let i = 0; i < tiposDeDocumento.length; i++) {
+        let opcTD = tiposDeDocumento[i];
+        let element = document.createElement("option");
+        element.textContent = opcTD.label;
+        element.value = opcTD.valor;
+        select.appendChild(element);
+    }
+}
+
+function actividadFisicaAdd() {
+    for (let i = 0; i < opcionesActividadFisica.length; i++) {
+        let opcAC = opcionesActividadFisica[i];
+        let element = document.createElement("option");
+        element.textContent = opcAC.label;
+        element.value = opcAC.valor;
+        selectActividad.appendChild(element);
+    }
+}
+
+tiposDocumento();
+actividadFisicaAdd();
